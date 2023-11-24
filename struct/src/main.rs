53ed479ac0895ff89b5214a71634acdfc2a2843b
Ant Skelton <ant@utto.com>
2023-11-24 14:49:41 +0000

    add new languages to greetings

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -1,9 +1,12 @@
 #[derive(Debug)]
 enum Lang {
-  English,
-  Spanish,
-  Chinese,
-  Texan,
+    English,
+    Spanish,
+    Chinese,
+    Texan,
+    Welsh,
+    French,
+    German,
 }
 
 struct Greeting {
@@ -12,18 +15,45 @@ struct Greeting {
 }
 
 fn main() {
-  let mut v :Vec<Greeting> = Vec::new();
+    let mut v: Vec<Greeting> = Vec::new();
 
-  let g : Greeting = Greeting { lang: Lang::English, message: String::from("Hello WasmEdge!") };
-  v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Spanish, message: String::from("Hola WasmEdge!") };
-  v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Texan, message: String::from("Howdy WasmEdge!") };
-  v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Chinese, message: String::from("WasmEdge 你好!") };
-  v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::English,
+        message: String::from("Hello WasmEdge!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::Spanish,
+        message: String::from("Hola WasmEdge!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::Texan,
+        message: String::from("Howdy WasmEdge!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::Chinese,
+        message: String::from("WasmEdge 你好!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::Welsh,
+        message: String::from("Bore da WasmEdge!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::French,
+        message: String::from("Bonjour WasmEdge!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::German,
+        message: String::from("Guten Tag WasmEdge!"),
+    };
+    v.push(g);
 
-  for e in v {
-    println!("{:?} {}", e.lang, e.message);
-  }
+    for e in v {
+        println!("{:?} {}", e.lang, e.message);
+    }
 }
